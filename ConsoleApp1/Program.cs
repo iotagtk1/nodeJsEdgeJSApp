9ef7c765bb752e348cc275dbc2c64f9d119aeeea
gtk <work.eggpop@gmail.com>
2021-10-20 17:13:54 +0900

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,20 +14,11 @@ namespace ConsoleApp1
             RestClient client = new RestClient();
             RestRequest request = new RestRequest();
             
-            // RestAPIから情報取得のためにアクセスする、e-StatのURLです(XML用)
-            client.BaseUrl = new Uri("http://api.e-stat.go.jp/rest/3.0/app/getStatsData");
-
             // RestAPIから情報取得のためにアクセスする、e-StatのURLです(json用) 
-            //client.BaseUrl = new Uri("http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData");
+            client.BaseUrl = new Uri("http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData");
 
             // HTTPのコマンドを指定します、情報の取得なので GET を指定します
             request.Method = Method.GET;
-
-            // e-Statで取得した appID をパラメータとして追加します
-            request.AddParameter("appId", "*** 取得したappidを記載する ***", ParameterType.GetOrPost);
-
-            // e-Statの統計表表示 ID (取得した統計表の番号) をパラメータとして追加します
-            request.AddParameter("statsDataId", "0003412317", ParameterType.GetOrPost);
 
             // リクエストを送信します
             var response = client.Execute(request);
